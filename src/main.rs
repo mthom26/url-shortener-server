fa08fce072b29427f65ddb362b13250e9440fbd8
mthom26 <mthom2626@gmail.com>
2019-04-22 22:20:30 +0100

    Add redis database and post_convert function

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,37 @@
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
-use rocket_contrib::json::JsonValue;
+use rocket_contrib::json::{JsonValue, Json};
+use rocket_contrib::databases::redis;
+
+#[database("redis_logs")]
+struct RedisDb(redis::Connection);
+
+#[derive(Deserialize)]
+struct RequestBody {
+    url: String
+}
 
 #[get("/")]
 fn index() -> JsonValue {
     json!({ "message": "Hello World!"})
 }
 
+#[post("/convert", data="<body>")]
+fn post_convert(
+    conn: RedisDb,
+    body: Json<RequestBody>
+) -> JsonValue {
+    let url = &body.url;
+    json!({ "message": format!("Received: {}", url) })
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .attach(RedisDb::fairing())
+        .mount("/", routes![index, post_convert])
+        .launch();
 }
